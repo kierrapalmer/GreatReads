41d9980223ae063dd9a0827cd4a94ee6b3562033
Kierra Palmer <kierrapalmer@weber.edu>
2018-10-26 11:47:59 -0600

    finish book review II

--- routes.py
+++ routes.py
@@ -1,21 +1,44 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-
-
-from books import books
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.testing import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+migrate = Migrate(app, db)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route("/")
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    imgUrl = db.Column(db.String(250))
+    description = db.Column(db.String(200))
+    author = db.Column(db.String(50))
+    genre = db.Column(db.String(50))
+    wantToRead = db.Column(db.Boolean)
+    friendRead = db.Column(db.Boolean)
+    featureBook = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return self.title
+
+
+books = Book.query.all()
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
   return render_template("index.html", books=books)
 
 
 @app.route("/genres")
 def genre():
-  return render_template("genres.html", books=books)
+    genre = request.args.get('genre', None)
+    return render_template("genres.html", books=books, genre=genre)
 
 
 @app.route("/book")
@@ -24,5 +47,13 @@ def book():
     book = books[bookid]
     return render_template("book.html", book=book)
 
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, Book=Book)
+
+
 if __name__ == "__main__":
   app.run(debug=True)
+
+
