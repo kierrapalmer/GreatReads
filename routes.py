7f9f33a36a1c03be993f0d1493a7a34b34510162
Kierra Palmer <kierrapalmer@weber.edu>
2018-12-14 16:29:37 -0700

    commit

--- routes.py
+++ routes.py
@@ -1,42 +1,65 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.testing import db
-from flask_migrate import Migrate
+import sqlite3
+
+PATH= 'books.db'
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-migrate = Migrate(app, db)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    imgUrl = db.Column(db.String(250))
-    description = db.Column(db.String(200))
-    author = db.Column(db.String(50))
-    genre = db.Column(db.String(50))
-    wantToRead = db.Column(db.Boolean)
-    friendRead = db.Column(db.Boolean)
-    featureBook = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return self.title
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection is None:
+        connection = g._connection = sqlite3.connect(PATH)
+        connection.row_factory = sqlite3.Row
+        return connection
 
 
-books = Book.query.all()
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
+#
+# class Book(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100))
+#     imgUrl = db.Column(db.String(250))
+#     description = db.Column(db.String(200))
+#     author = db.Column(db.String(50))
+#     genre = db.Column(db.String(50))
+#     wantToRead = db.Column(db.Boolean)
+#     friendRead = db.Column(db.Boolean)
+#     featureBook = db.Column(db.Boolean)
+#
+#     def __repr__(self):
+#         return self.title
+
+
+# books = Book.query.all()
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-  return render_template("index.html", books=books)
+    books = execute_sql('SELECT * FROM book')
+    return render_template("index.html", books=books)
 
 
 @app.route("/genres")
 def genre():
+    books = execute_sql('SELECT * FROM book')
     genre = request.args.get('genre', None)
     return render_template("genres.html", books=books, genre=genre)
 
@@ -44,13 +67,9 @@ def genre():
 @app.route("/book")
 def book():
     bookid = int(request.args.get('id', None))
-    book = books[bookid]
+    book = execute_sql('SELECT * FROM book WHERE id= ? LIMIT 1', (bookid,))
     return render_template("book.html", book=book)
 
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, Book=Book)
 
 
 if __name__ == "__main__":
